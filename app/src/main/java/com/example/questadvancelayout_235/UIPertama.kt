409e4fc260f48f5f538bf5b5706aaed6d4e7a764
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-10-23 13:44:53 +0700

    Inisialisasi: Menyiapkan struktur paket dan impor dasar.

---
+++ app/src/main/java/com/example/questadvancelayout_235/UIPertama.kt
@@ -0,0 +1,20 @@
+package com.example.myuipertama
+
+
+import com.example.questadvancelayout_235.R
+
+// Import yang diperlukan untuk Jetpack Compose
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
